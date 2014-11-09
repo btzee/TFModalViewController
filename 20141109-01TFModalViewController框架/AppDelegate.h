cfe0ce89d918211daba719c7a652f1ae2c9f68b1
朱佰通 <baitong@outlook.com>
2014-11-09 10:02:39 +0800

    Initial Commit

---
+++ 20141109-01TFModalViewController框架/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20141109-01TFModalViewController框架
+//
+//  Created by btz-mac on 14-11-9.
+//  Copyright (c) 2014年 朱佰通. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20141109-01TFModalViewController框架/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20141109-01TFModalViewController框架
+//
+//  Created by btz-mac on 14-11-9.
+//  Copyright (c) 2014年 朱佰通. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
